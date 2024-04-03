513102b91ded3a285d0463550c7af82081b781b4
user <user@DESKTOP-DETABAF>
2024-04-03 12:50:02 +0530

    hello

---
+++ C37_CoreJava/src/com/tns/dayone/FirstProgram.java
@@ -0,0 +1,32 @@
+package com.tns.dayone;
+
+public class FirstProgram {
+	private void employeedetails() {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome employees");
+
+	}
+	
+	private void employeeid() {
+		// TODO Auto-generated method stub
+		System.out.println("The employee id is:1728");
+
+	}
+	
+	private void employeeaddress() {
+		// TODO Auto-generated method stub
+		System.out.println("The employee address is:chennai");
+
+	}
+
+	public static void main(String[] args) {
+		
+		FirstProgram prog=new FirstProgram();
+		prog.employeedetails();
+		prog.employeeid();
+		prog.employeeaddress();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C37_CoreJava/src/com/tns/dayone/SecondProgram.java
@@ -0,0 +1,48 @@
+package com.tns.dayone;
+ class SecondProgram {
+
+	public static void main(String[] args) {
+		
+		//byte
+		
+		byte age=28;
+		System.out.println("The employee age is"+age);
+		
+		//short
+		
+		short pinnumber=4321;
+		System.out.println("The employee pin number is"+pinnumber);
+		
+		//int
+		int employeeid=12345678;
+		System.out.println("employeeid is"+employeeid);
+		
+		//long
+		
+		long phonenum=6382486917L;
+		System.out.println("The employee phone number is"+phonenum);
+		
+		//float
+		
+		float Salary=75000.75454f;
+		System.out.println("The employee salary is"+Salary);
+		
+		//double
+		
+		double Pf=455454.4524323432f;
+		System.out.println("The employee PF is:"+Pf);
+		
+		//char
+		
+		char sec='A';
+		System.out.println("The employee section is:"+sec);
+		
+		//string
+		
+		String email="dwaraka0531@gmail.com";
+		System.out.println("The employee email id is"+email);
+		
+		
+	}
+
+}
