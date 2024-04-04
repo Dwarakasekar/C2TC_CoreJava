646b95a2af09daa865bbf654c64caad62a2dc867
user <user@DESKTOP-DETABAF>
2024-04-04 14:47:57 +0530

    hello

---
+++ C37_CoreJava/src/com/tns/dayone/Runscoreds.java
@@ -0,0 +1,57 @@
+package com.tns.dayone;
+
+public class RunsScored {
+	
+	private String name;
+	private int age;
+	private int runs;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name= name;
+	}
+	//this - specific modifier  
+	
+	public  int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age=age;
+	}
+	
+	public int getRuns() {
+		return runs;
+	}
+	
+	public void setRuns(int runs) {
+		this.runs=runs;
+	}
+	
+	public String toString(){
+		return "CSK[Name ="+name+", Age="+age+",Runs="+runs+"]";
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		RunsScored ra=new RunsScored();
+		ra.setName("Dhoni");
+		ra.setAge(28);
+		ra.setRuns(183);
+		System.out.println(ra);
+		
+		/*System.out.println(ra.getName());
+		System.out.println(ra.getAge());
+		System.out.println(ra.getRuns());*/	
+		
+		
+	}
+
+	
+}
